637a744fbc449216c942b201746449923b4e9dbc
vasanth-exalogic <vasanth@exalogic.in>
2019-11-28 17:51:02 +0530

    3rd commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_uniqueness_of :email
   validates :email, presence:true
   validates :datebirth, presence:true
   validates :contact, presence:true
